583cf2ed0facb094e45df712994c02e5b4e6c919
zak-farrington <fritz@hax-studios.net>
2023-09-03 20:45:36 -0500

    add DataflowProcessor to better organize and isolate logic

--- Net7EtlBus.Service/Models/EtlBusImport.cs
+++ Net7EtlBus.Service/Models/EtlBusImport.cs
@@ -5,10 +5,10 @@ namespace Net7EtlBus.Service.Models
     public class EtlBusImport
     {
         public int Id { get; set; }
-        public required string FileName { get; set; }
-        public required string FileChecksum { get; set; }
+        public string? FileName { get; set; }
+        public string? FileChecksum { get; set; }
         public bool IsActive { get; set; } 
-        public string Status { get; set; }
+        public string? Status { get; set; }
 
         public DateTime? ImportStartTimeUtc { get; set; }
         public DateTime? EndDateTimeUtc { get; set;}
